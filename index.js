9fe48868f76ae3db7068bde3687d86074811c205
tom zhou <zs68j2ee@gmail.com>
2013-04-17 21:26:32 +0800

    fixed ws to tcp proxy

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 var Connect = require('connect'),
     Net = require('net'),
-    Buffer = require('buffer');
-    
+    Buffer = require('buffer').Buffer,
+    Fs = require('fs');
+
+
+// debug level
+var debug = 0;
+
 // VNC server info: {host: ..., port: ...} 
 var noVNC = module.exports = function(vnc){
     vnc = vnc || {};
@@ -17,38 +22,87 @@ var noVNC = module.exports = function(vnc){
     srv.use(Connect.static(__dirname));
     
     srv.use(function(req, res){
-        res.writeHeader(200);
-        res.end('Unknown path');
+        res.writeHeader(200, {'content-type', 'text/html'});
+        res.end(Fs.readFileSync(__dirname+'/novnc.html'));
     });
     
     // tcp proxy over ws
-    var tcproxy = function(ws){
+    var tcproxy = function(ws){    
         // create tcp connection to VNC server
         var ts = Net.connect(vnc, function(){
-            console.log('tcp connection...');
+            if (debug) console.log('tcp connection...');
             
             // relay data from ws to tcp
             ws.on('message', function(data, flags){
                 if (flags.binary) {
-                    console.log('binary ws message');
+                    if (debug) console.log('binary ws message');
                 } else {
                     data = new Buffer(data);
                 }
-                console.log('ws.onmessage...'+data.length+','+data.toString());
-                ts.write(data);
+                if (debug) console.log('ws.onmessage...'+data.length+','+data.toString());
+                
+                if (!ts.write(data)) {
+                    ws.pause();
+                    
+                    ts.once('drain', function(){
+                        ws.resume();
+                    });
+                    
+                    setTimeout(function(){
+                        ws.resume();
+                    }, 100); // 100ms 
+                }
             });
             ws.on('close', function(){
                 console.log('ws.onclose...');
                 ts.end();
             });
+            ws.on('error', function(){
+                console.log('ws.onerror...');
+                // send RFB error code
+                // TBD...
+                ts.end('error');
+            });
             
             // relay data from tcp to ws
             ts.on('data', function(data){
                 console.log('ts.ondata...'+data.length+','+data.toString());
-                ws.send(data, {binary: true});
+                if (ws.supports.binary) {
+                    if (!ws.send(data, {binary: true, mask: true})) {
+                        ts.pause();
+                        
+                        ws.on('drain', function(){
+                            ts.resume();
+                        });
+                        
+                        setTimeout(function(){
+                            ts.resume();
+                        }, 100); // 100ms 
+                    }
+                } else {                    
+                    if (!ws.send(ws.send(data.toString('base64'), {binary: false, mask: true})) {
+                        ts.pause();
+                        
+                        ws.on('drain', function(){
+                            ts.resume();
+                        });
+                        
+                        setTimeout(function(){
+                            ts.resume();
+                        }, 100); // 100ms 
+                    }
+                }
             });
             ts.on('end', function(){
                 console.log('ts.onend...');
+                ws.close();
+            });
+            ts.on('close', function(){
+                console.log('ts.onclose...');
+                ws.close();
+            });
+            ts.on('error', function(){
+                console.log('ts.onerror...');
                 ws.close();
             });
         });
