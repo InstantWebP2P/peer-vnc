edb2035ed8dd65a5c22c56f8d57f08cf7fdf4d78
tom zhou <zs68j2ee@gmail.com>
2013-04-17 08:40:23 +0800

    sync to noVNC upstream

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // noVNC port of Node.js, tom zhou <zs68j2ee@gmail.com>
 
 var Connect = require('connect'),
-    Net = require('net');
+    Net = require('net'),
+    Buffer = require('buffer');
     
 // VNC server info: {host: ..., port: ...} 
 var noVNC = module.exports = function(vnc){
@@ -27,22 +28,27 @@ var noVNC = module.exports = function(vnc){
             console.log('tcp connection...');
             
             // relay data from ws to tcp
-            ws.on('message', function(data){
-                console.log('ts.write...');
+            ws.on('message', function(data, flags){
+                if (flags.binary) {
+                    console.log('binary ws message');
+                } else {
+                    data = new Buffer(data);
+                }
+                console.log('ws.onmessage...'+data.length+','+data.toString());
                 ts.write(data);
             });
             ws.on('close', function(){
-                console.log('ts.end...');
+                console.log('ws.onclose...');
                 ts.end();
             });
             
             // relay data from tcp to ws
             ts.on('data', function(data){
-                console.log('ws.send...');
+                console.log('ts.ondata...'+data.length+','+data.toString());
                 ws.send(data, {binary: true});
             });
             ts.on('end', function(){
-                console.log('ws.close...');
+                console.log('ts.onend...');
                 ws.close();
             });
         });
@@ -61,14 +67,12 @@ var http = require('http'),
     WebSocket = require('wspp'),
     WebSocketServer = WebSocket.Server;
 
-var test = noVNC();
+var test = noVNC({host: '192.188.1.101', port: 5900});
 
 var srv = http.createServer(test.srv);
-var wss = new WebSocketServer({server: srv, path: 'tcp'});
-
-wss.on('connection', test.tcproxy);
-
 srv.listen(5600);
-
 console.log('noVNC proxy server listening on 5600');
+
+var wss = new WebSocketServer({server: srv, path: '/websockify'});
+wss.on('connection', test.tcproxy);
 
