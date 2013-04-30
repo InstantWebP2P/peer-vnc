712b5b051b846f0c420965af0ff63760a1d1f1ec
tom zhou <zs68j2ee@gmail.com>
2013-04-30 10:24:50 +0800

    fixed typo

--- examples/http-srv.js
+++ examples/http-srv.js
@@ -6,7 +6,7 @@ var noVNC = require('../novnc'),
     
 var srv = http.createServer(noVNC.webServer);
 srv.listen(5080);
-console.log('noVNC http server listening on 5600');
+console.log('noVNC http server listening on 5080');
 
 var wss = new WebSocketServer({server: srv, path: '/peervnc'});
 wss.on('connection', noVNC.tcpProxy({host: 'localhost', port: 5901}));
