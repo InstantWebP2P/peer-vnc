77c8a0a8b22c8963af296f4b3b44b0240fa3b2fc
tom zhou <zs68j2ee@gmail.com>
2013-06-01 19:23:06 +0800

    added usrkey in arguments of ./bin/peer-vnc

--- examples/proxy-local.js
+++ examples/proxy-local.js
@@ -2,8 +2,8 @@ var Proxy = require('../index');
 
 
 var server = new Proxy(['localhost:5901'], function(err, proxyURL){
-        console.log('VNC                   Proxy URL(please open it on browser)');
-        for (var k in proxyURL) {
-            console.log(k+'        '+proxyURL[k]);
-        }
-    });
+    console.log('VNC                   Proxy URL(please open it on browser)');
+    for (var k in proxyURL) {
+        console.log(k+'        '+proxyURL[k]);
+    }
+});
